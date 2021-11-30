13e4f576ad5855c97104bd3e9b8721cbd5d5f7e8
Matheus Patrick <mpatrickaires@gmail.com>
2021-11-30 16:47:29 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const App = props => {
+    return (
+        <SafeAreaView>
+            <Text></Text>
+        </SafeAreaView>
+    );
+};
+
+export default App;
